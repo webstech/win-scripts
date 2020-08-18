58eccf87f3510e77399de9fdebe363c10847b29f
Chris. Webster <chris@webstech.net>
2020-08-18 16:37:02 -0700

    Rebase main instead of master if present

--- src/rebase-master.cmd
+++ src/rebase-master.cmd
@@ -1,23 +1,26 @@
 @echo off
+@rem use main for master branch if present.
 @rem rebase master from upstream/master.
 @rem If the current branch is not master and it has no changes,
 @rem switch to master, rebase and switch back.
 
 setlocal enabledelayedexpansion
 
+set main=master
+FOR /F "usebackq" %%i IN (`git branch -l main`) DO set main=main
 FOR /F "usebackq" %%i IN (`git branch --show-current`) DO set br=%%i
-if /I .%br% neq .master (
+if /I .%br% neq .%main% (
 	@rem can't use git status -s |find /c " "
 	FOR /F "usebackq" %%i IN (`git status -s`) DO set ch=%%i
  	if /I .!ch! neq . (
-		echo Current branch %br% is not master and contains changes.
+		echo Current branch %br% is not %main% and contains changes.
 		goto :EOF
 	)
-	git checkout master
+	git checkout %main%
 )
 
-git pull -r upstream master
+git pull -r upstream %main%
 
-if /I .%br% neq .master (
+if /I .%br% neq .%main% (
 	git checkout %br%
 )
